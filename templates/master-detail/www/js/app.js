1f09d5ea7c6b19a339340b8bea6ef0319915bc7d
Fran Dios <frankdiox@gmail.com>
2015-05-07 16:27:03 +0900

    Templates updated

--- templates/master-detail/www/js/app.js
+++ templates/master-detail/www/js/app.js
@@ -4,7 +4,9 @@
 
   module.controller('AppController', function($scope, $data) {
     $scope.doSomething = function() {
-      ons.notification.alert({message: 'Hello, World!'});
+      setTimeout(function() {
+        ons.notification.alert({ message: 'tapped' });
+      }, 100);
     };
   });
 
@@ -13,8 +15,8 @@
   });
 
   module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    
+    $scope.items = $data.items;
+
     $scope.showDetail = function(index) {
       var selectedItem = $data.items[index];
       $data.selectedItem = selectedItem;
@@ -24,30 +26,30 @@
 
   module.factory('$data', function() {
       var data = {};
-      
+
       data.items = [
-          { 
+          {
               title: 'Item 1 Title',
               label: '4h',
               desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
           },
-          { 
+          {
               title: 'Another Item Title',
               label: '6h',
               desc: 'Ut enim ad minim veniam.'
           },
-          { 
+          {
               title: 'Yet Another Item Title',
               label: '1day ago',
               desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
           },
-          { 
+          {
               title: 'Yet Another Item Title',
               label: '1day ago',
               desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
           }
-      ]; 
-      
+      ];
+
       return data;
   });
 })();
